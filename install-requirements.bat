3c2bab427a66dfa6e6bb0ef6657d666a26a96fac
Lucy <lucy.puyenbroek@gmail.com>
2023-10-20 09:53:28 +0200

    View commit message for more details.
    Rewritten most of the script.
    - Removed the Git install (why was that needed?)
    - Removed the "extras" on the scoop install, since that is not needed as well.
    - Removed some of the "call" statements, was a bit messy...
    - Script not completely runs, but requires more testing to be approved.

--- install-requirements.bat
+++ install-requirements.bat
@@ -8,64 +8,16 @@ echo "###           MADE WITH LOVE BY 'Luciousdev'           ###"
 echo "###                   mika-linux.com                   ###"
 echo "##########################################################"
 
-REM Check if Git is installed
-git --version >nul 2>&1
-if errorlevel 1 (
-    echo [WARNING] - Git is not installed. Installing Git...
-    call winget install --id Git.Git -e --source winget
-    echo [OK] - Git is successfully installed.
-    
-    REM Refresh environment variables
-    call refreshenv
-    
-    
-) else (
-    echo [OK] - Git is already installed.
-    
-)
-
-REM Check if Scoop is installed
-scoop help >nul 2>&1
-if errorlevel 1  (
-    
-    echo [WARNING] - Scoop is not installed. Installing Scoop...
-    powershell -Command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser"
-    powershell -Command "iwr -useb get.scoop.sh | iex"
-    echo [OK] - Scoop is successfully installed.
-) else (
-    
-    echo [OK] - Scoop is already installed.
-)
-
-REM Install packages using Scoop
-echo "installing bucket.."
-call scoop bucket add extras
-
-REM Install ani-cli from source
-echo "installing ani-cli..."
-call scoop install ani-cli
-
-echo "installing aditional ani-cli packages.."
-REM Install additional packages
- 
-call scoop install aria2
-call scoop install ffmpeg
-call scoop install fzf
-call scoop install git
-call scoop install extras
-call scoop install mpv
-call scoop install wget
-
-
 REM Check if Python is installed
 python --version >nul 2>&1
 if errorlevel 1 (
     echo [WARNING] - Python is not installed. Installing Python...
-    call winget install python3
+    start /b winget install python3
     echo [OK] - Python is successfully installed.
 ) else (
     echo [OK] - Python is already installed.
 )
+
 
 REM Check if Pip is installed
 pip --version >nul 2>&1
@@ -76,6 +28,22 @@ if errorlevel 1 (
     echo [OK] - pip is already installed.
 )
 
+@REM Handling the scoop install
+powershell -Command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser"
+powershell -Command "iwr -useb get.scoop.sh | iex"
+echo [OK] - Scoop is successfully installed, or already present on users machine.
+
+REM Install packages using Scoop
+echo "installing bucket.."
+call scoop bucket add extras
+
+call refreshenv
+
+echo "installing ani-cli and other packages."
+REM Install additional packages
+call scoop install aria2 ffmpeg fzf mpv wget ani-cli
+
+
 REM Install packages from requirements.txt using Pip
 if exist requirements.txt (
     echo [PROGRESS] - Installing Python packages from requirements.txt...
@@ -84,10 +52,10 @@ if exist requirements.txt (
     set /p "arguments=Would you like to use the argument '--break-system-packages'? Note: this can cause major issues. [y/n]"
     if /i "%arguments%"=="y" (
         REM Install Python dependencies
-        call pip install --break-system-packages -r requirements.txt || call :handle_error
+        start /b pip install --break-system-packages -r requirements.txt || call :handle_error
     ) else (
         REM Install Python dependencies
-        call pip install -r requirements.txt || call :handle_error pip
+        start /b pip install -r requirements.txt || call :handle_error pip
     )
 ) else (
     echo [ERROR] - requirements.txt not found.
@@ -104,4 +72,3 @@ if /i "%argument%"=="y" (
 )
 
 pause
-
